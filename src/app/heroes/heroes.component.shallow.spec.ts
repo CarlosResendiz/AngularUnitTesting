14c66bad896487d35a463fa6b6376679341905cd
Carlos <resendiz.ca@gmail.com>
2019-11-10 22:06:46 -0600

    using DebugElement on heroes.component.shallow.spec.ts

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -2,6 +2,8 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { HeroComponent } from '../hero/hero.component';
 import { IterableChangeRecord_ } from '@angular/core/src/change_detection/differs/default_iterable_differ';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
 
 describe('HeroComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroComponent>;
@@ -23,6 +25,10 @@ describe('HeroComponent (shallow tests)', () => {
     fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
     fixture.detectChanges();
 
-    expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+    const deA = fixture.debugElement.query(By.css('a'));
+
+    expect(deA.nativeElement.textContent).toContain('SuperDude');
+
+    //  expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
   });
 });
